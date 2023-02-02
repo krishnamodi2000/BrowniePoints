d4018c0fad5ae5b60e2fa9363b88eee9e0f28afe
Roshil Ka Patel <rs622844@dal.ca>
2023-02-01 20:25:53 -0400

    Initilized frontend and backend.

---
+++ backend/src/main/java/com/GRP3/BPA/BpaApplication.java
@@ -0,0 +1,13 @@
+package com.GRP3.BPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/GRP3/BPA/BpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GRP3.BPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
