33c0c7bc7b73bbdac1f59d31812961ab5ff57251
Vaidik Nimavat <vd386827@dal.ca>
2023-02-13 16:40:52 -0400

    Adding JWT and Login functionality, Modified signup functionality.

---
+++ backend/src/main/java/com/GRP3/BPA/model/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.GRP3.BPA.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    private String email;
+    String password;
+}
--- backend/src/main/java/com/GRP3/BPA/service/UserServiceImpl.java
+++ backend/src/main/java/com/GRP3/BPA/service/UserServiceImpl.java
@@ -3,19 +3,47 @@ package com.GRP3.BPA.service;
 import com.GRP3.BPA.model.User;
 import com.GRP3.BPA.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserServiceImpl implements UserService{
 
+@Service
+public class UserServiceImpl implements UserDetailsService, UserService {
+
+    @Autowired
     private UserRepository userRepository;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository){
-        this.userRepository = userRepository;
+    private PasswordEncoder passwordEncoder;
+//    @Autowired
+//    public UserServiceImpl(UserRepository userRepository){
+//        super();
+//        this.userRepository = userRepository;
+//    }
+
+    public User saveUser(User user) {
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
+        return userRepository.save(user);
     }
 
-    public User saveUser(User user){
-        return userRepository.save(user);
+    @Override
+    public User login(String email, String password) throws UsernameNotFoundException{
+        User user = this.loadUserByUsername(email);
+
+        if(user == null) throw new UsernameNotFoundException("User does not exist");
+        if(user.getPassword().equals(password)) return user;
+
+        return null;
+    }
+
+    @Override
+    public User loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+
+        return user;
+
     }
 }
