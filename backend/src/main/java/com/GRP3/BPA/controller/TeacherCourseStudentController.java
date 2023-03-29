2d50ff8a9ddf634df79fa3e2e18bd6e5b3c9e58f
Roshil Amitbhai Ka Patel <rs622844@dal.ca>
2023-03-29 03:05:18 -0300

    Added the validation of teacher role in the teacher safe api's:

--- backend/src/main/java/com/GRP3/BPA/controller/TeacherCourseStudentController.java
+++ backend/src/main/java/com/GRP3/BPA/controller/TeacherCourseStudentController.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/teachers/courses")
+@PreAuthorize("hasRole('TEACHER')")
 public class TeacherCourseStudentController {
     @Autowired
     private CourseService courseService;
