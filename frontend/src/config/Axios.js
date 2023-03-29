215746825780d73a2e8bc021ea97d5c235cd5f92
Riya Patel <ry818465@dal.ca>
2023-03-28 22:51:22 -0300

    merge conflict reslove

--- frontend/src/config/Axios.js
+++ frontend/src/config/Axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const AxiosInstance = axios.create({
   baseURL: 'http://192.168.2.189:8080/api/',
-  // baseURL: 'http://10.0.2.2:8080/api/',
+  //baseURL: 'http://10.0.2.2:8080/api/',
 });
 
 AxiosInstance.interceptors.request.use(async request => {
