87ecb217e1b5122e2cd241b4ca31806774e57085
Riya Patel <ry818465@dal.ca>
2023-04-04 03:34:01 -0300

    Added Saga config for the Student

--- frontend/src/redux/saga.js
+++ frontend/src/redux/saga.js
@@ -2,7 +2,8 @@ import {all} from 'redux-saga/effects';
 import courseSaga from './course/saga';
 import pointsSaga from './points/saga';
 import userSaga from './user/saga';
+import studentSaga from './student/saga';
 
 export default function* rootSaga() {
-  yield all([userSaga(), pointsSaga(), courseSaga()]);
+  yield all([userSaga(), pointsSaga(), courseSaga(), studentSaga()]);
 }
