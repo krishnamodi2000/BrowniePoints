f082026f075c8438536b4015fb263c47386fb8d6
sarthakpatel <sr555161@dal.ca>
2023-04-08 02:19:58 -0300

    Issue Python File changes made.

--- backend/.gitlab/issue.py
+++ backend/.gitlab/issue.py
@@ -7,7 +7,7 @@ path_to_smells = "smells/"
 smell_files = [f for f in os.listdir(path_to_smells) if os.path.isfile(os.path.join(path_to_smells,f)) and str(f).strip().endswith(".csv")]
 
 headers = {
-    "PRIVATE-TOKEN": "glpat-xcRnwSex49Gmht_SKscT"
+    "PRIVATE-TOKEN": "glpat-VG8aA4xrWEinDTN7oV6h"
 }
 for sf in smell_files:
 
@@ -21,7 +21,7 @@ for sf in smell_files:
     body={
         "description":{raw_md}
     }
-    gitlab_output = requests.post(f"https://git.cs.dal.ca/api/v4/projects/{69230}/issues?{query_params}",headers=headers, data=body)
+    gitlab_output = requests.post(f"https://git.cs.dal.ca/api/v4/projects/{67465}/issues?{query_params}",headers=headers, data=body)
     if not gitlab_output.status_code == 201:
         raise Exception
 
